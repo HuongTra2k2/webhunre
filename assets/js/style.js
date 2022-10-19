c176836f3492d2ffef1f1d9af9f7c69c75af7eef
Huong Tra <20111061199@hunre.edu.vn>
2022-10-19 09:30:53 +0700

    first commit

---
+++ assets/js/style.js
@@ -0,0 +1,13 @@
+
+
+
+$(document).ready(function(){
+  $('.slider2').slick({
+    slidesToShow: 5,
+  slidesToScroll: 1,
+  prevArrow:"<button type='button' class='btn slick-prev pull-left'><i class='fa fa-angle-left' aria-hidden='true'></i></button>",
+            nextArrow:"<button type='button' class='btn slick-next pull-right'><i class='fa fa-angle-right' aria-hidden='true'></i></button>",
+            autoplay: true,
+  autoplaySpeed: 2000,
+  });
+});
